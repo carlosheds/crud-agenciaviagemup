724445cb6f20b8edabf5c5baccd7aa32f9e6c664
carlosheds <carlos.heds@gmail.com>
2022-02-06 09:35:46 -0300

    Contato DAO added. Passagem model added cliente and destino

---
+++ src/br/com/agenciaup/dao/ClienteDAO.java
@@ -0,0 +1,44 @@
+package br.com.agenciaup.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.com.agenciaup.factory.AgenciaFactory;
+import br.com.agenciaup.model.Cliente;
+
+public class ClienteDAO {
+	public void save(Cliente cliente) {
+		String sql = "INSERT INTO cliente(nome,idade)" + " VALUES (?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = AgenciaFactory.createConnectionSQLServer();
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setString(1, cliente.getNome());
+			pstm.setInt(2, cliente.getIdade());
+			
+			pstm.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void removeById(int id) {
+		
+	}
+}
+
--- src/br/com/agenciaup/model/Passagem.java
+++ src/br/com/agenciaup/model/Passagem.java
@@ -2,6 +2,8 @@ package br.com.agenciaup.model;
 
 public class Passagem {
 	private int id_cliente,id_destino;
+	private Cliente cliente;
+	private Destino destino;
 
 	public int getId_cliente() {
 		return id_cliente;
@@ -17,6 +19,22 @@ public class Passagem {
 
 	public void setId_destino(int id_destino) {
 		this.id_destino = id_destino;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Destino getDestino() {
+		return destino;
+	}
+
+	public void setDestino(Destino destino) {
+		this.destino = destino;
 	}
 	
 	
