4719fb1c0f89f999610bd5ea27a6314329add01b
carlosheds <carlos.heds@gmail.com>
2022-02-06 08:58:14 -0300

    Passagem model added

---
+++ src/br/com/agenciaup/model/Passagem.java
@@ -0,0 +1,23 @@
+package br.com.agenciaup.model;
+
+public class Passagem {
+	private int id_cliente,id_destino;
+
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public int getId_destino() {
+		return id_destino;
+	}
+
+	public void setId_destino(int id_destino) {
+		this.id_destino = id_destino;
+	}
+	
+	
+}
