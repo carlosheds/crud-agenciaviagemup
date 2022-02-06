6fefb78e530d88fb9013e04c69504becfe0a741f
carlosheds <carlos.heds@gmail.com>
2022-02-06 08:55:02 -0300

    Destino model added

---
+++ src/br/com/agenciaup/model/Destino.java
@@ -0,0 +1,30 @@
+package br.com.agenciaup.model;
+
+public class Destino {
+	private String pais,estado,cidade;
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+}
